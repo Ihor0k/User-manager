671c116acceff33a9ae795dbc52ca812899e651a
Ihor Belenok <ihor.belenok@area9.dk>
2018-06-05 11:09:22 +0300

    Fix war packaging for tomcat

--- src/main/java/my/app/UserManagerApp.java
+++ src/main/java/my/app/UserManagerApp.java
@@ -2,9 +2,16 @@ package my.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UserManagerApp {
+public class UserManagerApp extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(UserManagerApp.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(UserManagerApp.class);
